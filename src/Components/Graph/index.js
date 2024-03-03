51104bdbd831a5b746752f1041b34446ea59c603
Agastinmellimi <125130023+Agastinmellimi@users.noreply.github.com>
2024-03-03 19:07:41 +0530

    label css

--- src/Components/Graph/index.js
+++ src/Components/Graph/index.js
@@ -115,7 +115,7 @@ export const Graph = () => {
                     <Tooltip trigger='hover' cursor={{fill: 'transparent'}}  contentStyle={{fontWeight: 'bold', fontFamily: "Mandali", backgroundColor: "rgba(193, 221, 139, 0.81)", backdropFilter: 'blur(3px)', color: "#fff", borderRadius: '6px'}}/>
                     <Legend />
                     <XAxis type='category' dataKey='name' tick={barHide === true ? {fill: "#fff"} : false} stroke="#f5d86e"   interval={0} style={{fontSize: 10, fontFamily: "Mandali, sans-serif", fontStyle: 'italic'}}/>
-                    <Bar dataKey="presents" label={{dataKey:'name', position: 'insideUp', angle: '-90', stroke: "#f5d86e", fontSize: '12px', letterSpacing: '0.20em'}} fill="#097569" maxBarSize={45}   style={{cursor: "pointer", fontSize: 8, fontFamily: "Mandali, sans-serif"}} />
+                    <Bar dataKey="presents" label={barHide === false && {dataKey:'name', position: 'insideUp', angle: '-90', stroke: "#f5d86e", fontSize: '12px', letterSpacing: '0.20em'}} fill="#097569" maxBarSize={45}   style={{cursor: "pointer", fontSize: 8, fontFamily: "Mandali, sans-serif"}} />
                 </BarChart>
         </ResponsiveContainer>
         
